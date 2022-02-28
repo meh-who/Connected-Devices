a5d59aaee7c5b10c58852fea59d2eb843445d929
meh-who <meijiehuuu@gmail.com>
2022-02-28 00:10:54 -0500

    gamma correction added

--- Midterm_SkylightClock/Arduino_SkylightClock/Arduino_SkylightClock.ino
+++ Midterm_SkylightClock/Arduino_SkylightClock/Arduino_SkylightClock.ino
@@ -1,5 +1,13 @@
-//real-time LED Clock
+/*Skylight Clock 
+ * Meijie Hu & Zeshu Zhu
+ * 
+ * A skylight clock that imitate the color of the sky
+ * 
+ * question: how to achieve desaturated blue?
+ * 
+ */
 #include "RTClib.h"
+#include "gammaCorrection.h"
 #include <FastLED.h>
 #define NUM_LEDS 120
 
@@ -14,9 +22,9 @@ int h = 0;
 int m = 0;
 int s = 0;
 
-static uint8_t hue_h;
-static uint8_t hue_m;
-static uint8_t hue_s;
+CRGB color_h = CHSV(140, 255, 200);
+CRGB color_m = CRGB(gamma8[27],gamma8[42],gamma8[60]);
+CRGB color_s = CRGB(gamma8[240],gamma8[148],gamma8[56]);
 
 unsigned long previousMillis;
 
@@ -55,6 +63,21 @@ void setup () {
   Serial.print(':');
   Serial.print(s, DEC);
   Serial.println();
+
+  
+  // pre-display the time on LED
+  static uint8_t brightness = 255;
+  for (int i = s*2; i > s*2 - 5; i--) {
+    brightness -= 50;
+    leds_s[i % NUM_LEDS] = CHSV(50, 255, brightness);
+  }
+  brightness = 150;
+  for (int j = m*2; j > m*2 - 5; j--) {
+    brightness -= 30;
+    leds_m[j % NUM_LEDS] = CHSV(170, 255, brightness);
+  }
+  FastLED.show(); 
+  
 }
 
 void loop () {
@@ -62,30 +85,30 @@ void loop () {
   for (int i = 0; i < NUM_LEDS;) {
     for (int j = 0; j < NUM_LEDS;) {
       for (int k = 0; k < NUM_LEDS;) {
-        if (millis() - previousMillis >= 500) {  // update s hand per 500ms
+        if (millis() - previousMillis >= 50) {  // update s hand per 500ms
           previousMillis = millis();
-          leds_s.fadeToBlackBy(100);
-          leds_s[(k + s * 2) % NUM_LEDS] = CHSV(20, 255, 200);
+          leds_s.fadeToBlackBy(50);
+          leds_s[(k + s * 2) % NUM_LEDS] = color_s;
           k++;   // advance one s hand led per update
           if (k == 60) {  
-            leds_m.fadeToBlackBy(50);
-            leds_m[(j + m * 2) % NUM_LEDS] = CHSV(80, 255, 200);
+            leds_m.fadeToBlackBy(20);
+            leds_m[(j + m * 2) % NUM_LEDS] = color_m;
             j++;   // advance one m hand led when s hand finish half a loop
           }
           FastLED.show();   // display LED per update
         }
       }
-      leds_m.fadeToBlackBy(50);
-      leds_m[(j + m * 2) % NUM_LEDS] = CHSV(80, 255, 200);
+      leds_m.fadeToBlackBy(20);
+      leds_m[(j + m * 2) % NUM_LEDS] = color_m;
       j++;   // advance one m hand led when s hand finish a loop
       if (j == 60) {  
         leds_h.fadeToBlackBy(50);
-        leds_h[(i + m * 2) % NUM_LEDS] = CHSV(80, 255, 200);
+        leds_h[(i + h * 2) % NUM_LEDS] = color_h;
         i++;   // advance one h hand led when m hand finish half a loop
       }
     }
     leds_h.fadeToBlackBy(50);
-    leds_h[(i + h * 2) % NUM_LEDS] = CHSV(140, 255, 200);
+    leds_h[(i + h * 2) % NUM_LEDS] = color_h;
     i++;   // advance one h hand led when m hand finish a loop
   }
 
