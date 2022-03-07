3cc397f687b78aacaeb7aa40223604c956819830
meh-who <meijiehuuu@gmail.com>
2022-03-07 15:36:33 -0500

    add web

---
+++ Midterm_SkylightClock/web/function.js
@@ -0,0 +1,94 @@
+function showTime(){
+  var date = new Date();
+  var h = date.getHours(); 
+  var m = date.getMinutes(); 
+  var s = date.getSeconds(); 
+  // （如果要显示秒针umcommon这个）
+  var session = "AM";
+  
+  if(h == 0){
+      h = 12;
+  }
+  
+  if(h > 12){
+      h = h - 12;
+      session = "PM";
+  }
+  
+  h = (h < 10) ? "0" + h : h;
+  // （arduino使用时针数据）
+  m = (m < 10) ? "0" + m : m;
+  // （arduino使用分针数据）
+  s = (s < 10) ? "0" + s : s;
+  // （如果要显示秒针umcommon这个）
+  
+  var time = h + ":" + m + ":" + s + " " + session;
+  // （如果要显示秒针用这个）
+  // var time = h + ":" + m + " " + session;
+  document.getElementById("DigitalCLOCK").innerText = time;
+  document.getElementById("DigitalCLOCK").textContent = time;
+  
+  setTimeout(showTime, 1000);
+  
+}
+
+showTime();
+
+
+var $picked = $("#picked"); // 预览挑选的颜色
+var canvas = $('#canvas_picker')[0];
+var context = canvas.getContext('2d');
+var counter =0;
+
+
+$("#file_upload").change(function (e) {
+var F = this.files[0];
+var reader = new FileReader();
+reader.onload = imageIsLoaded;
+reader.readAsDataURL(F);  
+});
+
+function imageIsLoaded(e) {
+var img = new Image();
+img.onload = function(){
+  canvas.width  = 500;    // 调整画布大小,相对于图像大小
+  canvas.height = 500*this.height/this.width; 
+  context.drawImage(this, 0, 0, canvas.width, canvas.height); // 在 0, 0画图, not 10, 10
+};
+img.src = e.target.result;
+}
+
+$('#canvas_picker').click(function(event){
+var x = event.pageX - $(this).offset().left; // 相对于canvas offs固定坐标
+var y = event.pageY - $(this).offset().top;
+var img_data = context.getImageData(x,y , 1, 1).data;
+window.img_data = img_data;
+var R = img_data[0];
+var G = img_data[1];
+var B = img_data[2]; 
+var rgb = R + ',' + G + ',' + B ;
+var hex = rgbToHex(R,G,B);
+$('#rgb input').val( rgb );
+$('#hex input').val('#' + hex);
+if(counter<=2){
+$picked.append("<span id='zeshu' style='background:#"+hex+"'>"+rgb+"</span>");
+counter++;
+}
+if(counter>2){
+$("span").remove();
+counter =0;
+}
+
+// testme()
+});
+
+function rgbToHex(R, G, B) {
+return toHex(R) + toHex(G) + toHex(B);
+}
+
+function toHex(n) {
+n = parseInt(n, 10);
+if (isNaN(n))  return "00";
+n = Math.max(0, Math.min(n, 255));
+return "0123456789ABCDEF".charAt((n - n % 16) / 16)  + "0123456789ABCDEF".charAt(n % 16);
+}
---
+++ Midterm_SkylightClock/web/script.js
@@ -0,0 +1,97 @@
+// advertised service UUID of the  to search for:
+const serviceUuid = '26b68f48-bddc-42fa-8a03-38be7b2bfc6e';
+// DOM elements to interact with:
+let connectButton;
+let dataDiv;
+let deviceDiv;
+let ledButton;
+// TODO these could be one JSON object representing the device
+// and its services and characteristics:
+let myDevice;
+let myCharacteristic;
+
+
+// this function is called when the page is loaded. 
+// event listener functions are initialized here:
+function setup() {
+  // put the DOM elements into global variables:
+  connectButton = document.getElementById('connect');
+  connectButton.addEventListener('click', connectToBle);
+  ledButton = document.getElementById('canvas_picker');
+  ledButton.addEventListener('click', writeToCharacteristic);
+  deviceDiv = document.getElementById('device');
+  dataDiv = document.getElementById('data');
+}
+
+// connect to the peripheral:
+function connectToBle() {
+  // options let you filter for a peripheral 
+  // with a particular service UUID:
+  let options = {
+    filters: [{
+      services: [serviceUuid]
+    }]
+  };
+  // start scanning:
+  navigator.bluetooth.requestDevice(options)
+    // when you get a device:
+    .then(device => {
+      myDevice = device;
+    //   deviceDiv.innerHTML = "Device name: " + device.name;
+    //   deviceDiv.innerHTML += "<br>Service UUID: " + serviceUuid;
+      return device.gatt.connect();
+    })
+    // get the primary service:
+    .then(server => server.getPrimaryService(serviceUuid))
+    .then(service => service.getCharacteristics())
+    // get the characteristics of the service:
+    .then(characteristics => readCharacteristics(characteristics))
+    // if there's an error:
+    .catch(error => console.log('Connection failed!', error));
+
+  function readCharacteristics(characteristics) {
+    // add the characterisitic UUID to the device div:
+    deviceDiv.innerHTML += "<br>Connected " ;
+    myCharacteristic = characteristics[0];
+    // Get the initial value:
+    characteristics[0].readValue()
+      .then(data => {
+        let ledState = data.getUint8(0);
+        ledButton.checked = ledState;
+      });
+  }
+}
+
+// write to a characteristic:
+function writeToCharacteristic() {
+  // LED state is whether or not the button is checked:
+  // let ledState = event.target.checked;
+  // if you're connected, write to the peripheral:
+  if (myDevice && myDevice.gatt.connected) {
+    // convert to an ArrayBufferView:
+    // var element = document.getElementById("zeshu");
+    // alert(element.innerHTML);
+    // let valueToSend = Int16Array.of(element.innerHTML);
+    // debugger
+    // alert(valueToSend);
+    
+    var r = window.img_data[0];
+    var g = window.img_data[1];
+    var b = window.img_data[2];
+    console.log(r);
+    // write to characteristic now:
+    // myCharacteristic.writeValue(valueToSend);
+    myCharacteristic.writeValue(new Uint8Array([r,g,b,0]));
+  }
+  // alert(zeshu);
+}
+
+// function testme(){
+//   alert("hello");
+//   var element = document.getElementById("zeshu");
+//   alert(element.innerHTML);
+// }
+
+// This is a listener for the page to load.
+// This is the command that actually starts the script:
+window.addEventListener('DOMContentLoaded', setup);
